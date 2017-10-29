decd72edb4f1bf2118ec0429bd272e773b78f7b3
Tommy Rodriguez <trodriguez91@icloud.com>
2017-10-29 15:31:07 -0700

    make sure to encode the auth token

--- src/Request/User.elm
+++ src/Request/User.elm
@@ -1,14 +1,16 @@
 module Request.User exposing (storeSession)
+import Data.AuthToken as AuthToken exposing (AuthToken, fallback)
 
 import Json.Encode as Encode
 import Ports
 
-storeSession : { r | email : String } -> Cmd msg
-storeSession { email } =
+storeSession : { r | email : String, token : AuthToken } -> Cmd msg
+storeSession { email, token } =
     let
         encodedUser =
             Encode.object
-                [ ("email", Encode.string email) ]
+                [ ("email", Encode.string email)
+                , ("authToken", AuthToken.encode token) ]
 
     in
         encodedUser
