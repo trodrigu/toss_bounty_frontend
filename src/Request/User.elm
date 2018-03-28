2d057cd7dad66aa5f53a7602ccae023c553a9dab
trodrigu <trodriguez91@icloud.com>
2018-03-27 21:00:28 -0700

    make sure to put role into the storage

--- src/Request/User.elm
+++ src/Request/User.elm
@@ -1,20 +1,22 @@
 module Request.User exposing (storeSession)
-import Data.AuthToken as AuthToken exposing (AuthToken, fallback)
 
+import Data.AuthToken as AuthToken exposing (AuthToken, fallback)
 import Json.Encode as Encode
 import Ports
 
-storeSession : { r | email : String, token : AuthToken, userId : String } -> Cmd msg
-storeSession { email, token, userId } =
+
+storeSession : { r | email : String, token : AuthToken, userId : String, role : Int } -> Cmd msg
+storeSession { email, token, userId, role } =
     let
         encodedUser =
             Encode.object
-                [ ("email", Encode.string email)
-                , ("authToken", AuthToken.encode token)
-                , ("userId", Encode.string userId) ]
-
+                [ ( "email", Encode.string email )
+                , ( "authToken", AuthToken.encode token )
+                , ( "userId", Encode.string userId )
+                , ( "role", Encode.int role )
+                ]
     in
-        encodedUser
+    encodedUser
         |> Encode.encode 0
         |> Just
         |> Ports.storeSession
