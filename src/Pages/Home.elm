65c7fb8e95417ee2771a2b3f36a3050e8bc9ea8a
Tommy Rodriguez <thomas.m.rodriguez@kp.org>
2018-06-04 14:34:24 -0700

    add in svg logo

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -4,7 +4,8 @@ import Html exposing (..)
 import Html.Attributes exposing (class, style)
 import RemoteData exposing (RemoteData(..), WebData)
 import Util exposing ((=>))
-
+import Svg exposing (Svg, circle, svg, rect)
+import Svg.Attributes exposing (fill, cx, cy, r, width, color, rx, ry, x, y, viewBox, height)
 
 type alias Model =
     { url : WebData GitHubUrl }
@@ -33,6 +34,15 @@ update msg model =
         NoOp ->
             ( model, Cmd.none ) => ExternalNoOp
 
+tossBountyLogo : Html.Html msg
+tossBountyLogo =
+    svg
+      [ width "120", height "120", fill "white" ]
+      [ rect [  x (toString 10 ), y (toString 50 ), width ( toString ( 30 * 0.5 ) ), height ( toString ( 0.5 * 10 ) ) ] []
+      , rect [  x (toString 10 ), y (toString 50 ), width ( toString ( 10 * 0.5 ) ), height ( toString ( 0.5 * 45 ) ) ] []
+      , circle [ r ( toString ( 10 * 0.5 ) ), cx (toString 35 ), cy (toString 55 ) ] []
+      , circle [ r ( toString ( 10 * 0.5 ) ), cx (toString 35 ), cy (toString 69 ) ] []
+      ]
 
 viewButton : Model -> Html Msg
 viewButton model =
@@ -58,27 +68,57 @@ viewButton model =
 
 view : Model -> Html Msg
 view model =
-    section [ class "hero is-primary is-bold is-large" ]
-        [ div [ class "hero-body" ]
-            [ div [ class "container" ]
-                [ div [ class "columns is-vcentered" ]
-                    [ div [ class "column has-text-centered" ]
-                        [ p [ class "title is-large" ] [ text "TossBounty" ]
-                        , p [ class "title" ]
-                            [ text "For the godmode Maintainer" ]
-                        , p [ class "title" ]
-                            [ text "...get some "
-                            , strong [] [ text " cash" ]
-                            ]
-                        , p [ class "title" ]
-                            [ text "For the super duper supporter" ]
-                        , p [ class "title" ]
-                            [ text "...keep those projects "
-                            , strong [] [ text " alive" ]
-                            ]
-                        , viewButton model
-                        ]
+    div []
+    [ section [ class "hero is-primary is-medium" ]
+        [ div [ class "hero-head" ]
+              [ nav [ class "navbar" ]
+                    [ div [ class "container" ]
+                          [ div [ class "navbar-brand" ]
+                                [ tossBountyLogo
+                                , span [ class "navbar-burger burger" ]
+                                      [ span [] []
+                                      , span [] []
+                                      , span [] []
+                                      ]
+                                ]
+                          , div [ class "navbar-menu" ]
+                                [ div [ class "navbar-end" ]
+                                      [ a [ class "navbar-item" ]
+                                          [ text "About" ]
+                                      , a [ class "navbar-item" ]
+                                          [ text "Login" ]
+                                      ]
+                                ]
+                          ]
                     ]
-                ]
+              ]
+        , div [ class "hero-body" ]
+            [ div [ class "container" ]
+                          [ div [ class "columns is-vcentered" ]
+                                [ div [ class "column is-6" ]
+                                      [ div [ class "section-header"]
+                                            [ h1 [ class "title" ]
+                                                  [ text "Easy crowdfunding for open source" ]
+                                            , h2 [ class "subtitle is-3" ]
+                                                [ text "Support the projects you love."]
+                                            , viewButton model
+                                            ]
+                                      ]
+                                ]
+                          ]
             ]
         ]
+    , section [ class "hero is-medium" ]
+        [ div [ class "hero-body" ]
+              [ div [ class "container" ]
+                    [ div [ class "columns" ]
+                          [ div [ class "column is-4 is-offset-1"]
+                                []
+                          , div [ class "column is-5 is-offset-1"]
+                              []
+                          ]
+                    ]
+              ]
+
+        ]
+    ]
