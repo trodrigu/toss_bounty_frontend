c15f8cb742d51e544f25617d56ca96c7ceb41cad
Tommy Rodriguez <thomas.m.rodriguez@kp.org>
2018-07-17 11:09:58 -0700

    rm the hero head

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -3,9 +3,10 @@ module Pages.Home exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (class, style)
 import RemoteData exposing (RemoteData(..), WebData)
+import Svg exposing (Svg, circle, rect, svg)
+import Svg.Attributes exposing (color, cx, cy, fill, height, r, rx, ry, viewBox, width, x, y)
 import Util exposing ((=>))
-import Svg exposing (Svg, circle, svg, rect)
-import Svg.Attributes exposing (fill, cx, cy, r, width, color, rx, ry, x, y, viewBox, height)
+
 
 type alias Model =
     { url : WebData GitHubUrl }
@@ -34,15 +35,17 @@ update msg model =
         NoOp ->
             ( model, Cmd.none ) => ExternalNoOp
 
+
 tossBountyLogo : Html.Html msg
 tossBountyLogo =
     svg
-      [ width "120", height "120", fill "white" ]
-      [ rect [  x (toString 10 ), y (toString 50 ), width ( toString ( 30 * 0.5 ) ), height ( toString ( 0.5 * 10 ) ) ] []
-      , rect [  x (toString 10 ), y (toString 50 ), width ( toString ( 10 * 0.5 ) ), height ( toString ( 0.5 * 45 ) ) ] []
-      , circle [ r ( toString ( 10 * 0.5 ) ), cx (toString 35 ), cy (toString 55 ) ] []
-      , circle [ r ( toString ( 10 * 0.5 ) ), cx (toString 35 ), cy (toString 69 ) ] []
-      ]
+        [ width "120", height "120", fill "white" ]
+        [ rect [ x (toString 10), y (toString 50), width (toString (30 * 0.5)), height (toString (0.5 * 10)) ] []
+        , rect [ x (toString 10), y (toString 50), width (toString (10 * 0.5)), height (toString (0.5 * 45)) ] []
+        , circle [ r (toString (10 * 0.5)), cx (toString 35), cy (toString 55) ] []
+        , circle [ r (toString (10 * 0.5)), cx (toString 35), cy (toString 69) ] []
+        ]
+
 
 viewButton : Model -> Html Msg
 viewButton model =
@@ -69,56 +72,33 @@ viewButton model =
 view : Model -> Html Msg
 view model =
     div []
-    [ section [ class "hero is-primary is-medium" ]
-        [ div [ class "hero-head" ]
-              [ nav [ class "navbar" ]
-                    [ div [ class "container" ]
-                          [ div [ class "navbar-brand" ]
-                                [ tossBountyLogo
-                                , span [ class "navbar-burger burger" ]
-                                      [ span [] []
-                                      , span [] []
-                                      , span [] []
-                                      ]
+        [ section [ class "hero is-primary is-medium" ]
+            [ div [ class "hero-body" ]
+                [ div [ class "container" ]
+                    [ div [ class "columns is-vcentered" ]
+                        [ div [ class "column is-6" ]
+                            [ div [ class "section-header" ]
+                                [ h1 [ class "title" ]
+                                    [ text "Easy crowdfunding for open source" ]
+                                , h2 [ class "subtitle is-3" ]
+                                    [ text "Support the projects you love." ]
+                                , viewButton model
                                 ]
-                          , div [ class "navbar-menu" ]
-                                [ div [ class "navbar-end" ]
-                                      [ a [ class "navbar-item" ]
-                                          [ text "About" ]
-                                      , a [ class "navbar-item" ]
-                                          [ text "Login" ]
-                                      ]
-                                ]
-                          ]
+                            ]
+                        ]
                     ]
-              ]
-        , div [ class "hero-body" ]
-            [ div [ class "container" ]
-                          [ div [ class "columns is-vcentered" ]
-                                [ div [ class "column is-6" ]
-                                      [ div [ class "section-header"]
-                                            [ h1 [ class "title" ]
-                                                  [ text "Easy crowdfunding for open source" ]
-                                            , h2 [ class "subtitle is-3" ]
-                                                [ text "Support the projects you love."]
-                                            , viewButton model
-                                            ]
-                                      ]
-                                ]
-                          ]
+                ]
+            ]
+        , section [ class "hero is-medium" ]
+            [ div [ class "hero-body" ]
+                [ div [ class "container" ]
+                    [ div [ class "columns" ]
+                        [ div [ class "column is-4 is-offset-1" ]
+                            []
+                        , div [ class "column is-5 is-offset-1" ]
+                            []
+                        ]
+                    ]
+                ]
             ]
         ]
-    , section [ class "hero is-medium" ]
-        [ div [ class "hero-body" ]
-              [ div [ class "container" ]
-                    [ div [ class "columns" ]
-                          [ div [ class "column is-4 is-offset-1"]
-                                []
-                          , div [ class "column is-5 is-offset-1"]
-                              []
-                          ]
-                    ]
-              ]
-
-        ]
-    ]
