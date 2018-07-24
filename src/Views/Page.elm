feb07bda8992f7fda5abc3a2e5c428be81c41822
Tommy Rodriguez <thomas.m.rodriguez@kp.org>
2018-07-24 11:11:09 -0700

    make footer href to create campaign route

--- src/Views/Page.elm
+++ src/Views/Page.elm
@@ -88,7 +88,7 @@ footerArea page =
                                 ]
                             ]
                         ]
-                    , a [ class "link", Router.href CreateUserRoleRoute ]
+                    , a [ class "link", Router.href CreateCampaignRoute ]
                         [ text "Start a campaign!" ]
                     , p []
                         [ a [ class "link", Router.href AboutRoute ]
